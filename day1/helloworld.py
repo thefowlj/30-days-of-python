d4740c9a4e74a1c9554b2d1c60ac56e46553c142
jonfowler <jfowler27@gmail.com>
2020-01-03 04:34:47 -0700

    Initial commit

---
+++ day1/helloworld.py
@@ -0,0 +1,23 @@
+# helloworld.py
+
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(5 % 3)
+print(5 / 3)
+print(5 ** 3)
+print(5 // 2)
+
+print('Jon')
+print('Fowler')
+print('USA')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh','Ptyhon','Finland']))
+print(type('Jon'))
+print(type('Fowler'))
+print(type('USA'))
