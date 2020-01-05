6e5eadbe4df9a0866a2d5b4076a2312ccf1d75d8
jonfowler <jfowler27@gmail.com>
2020-01-05 09:03:20 -0700

    Add day2

---
+++ day2/variables.py
@@ -0,0 +1,58 @@
+# Day 2: 30 Days of Python programming
+first_name = 'Jon'
+last_name = 'Fowler'
+full_name = first_name + ' ' + last_name
+country = 'USA'
+city = 'Denver'
+age = 32
+year = 2020
+is_married = True
+is_true = True
+is_light_on = is_true
+a, b, c = 1, 2, 3
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a))
+print(type(b))
+print(type(c))
+print(len(first_name))
+print(len(first_name) > len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+print(total)
+print(diff)
+print(product)
+print(division)
+print(remainder)
+print(exp)
+print(floor_division)
+
+r = float(input('Enter radius: '))
+PI = 3.14
+area_of_circle = PI * (r ** 2)
+circum_of_circle = 2 * PI * r
+print('Area: ' + str(area_of_circle))
+print('Circumference: ' + str(circum_of_circle))
+
+first_name = input('Enter First Name: ')
+last_name = input('Enter Last Name: ')
+country = input('Enter Country: ')
+age = input('Age: ')
+
+help('keywords')
